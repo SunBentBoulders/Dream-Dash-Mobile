96969a069c77f15d0ce2a22909f5a7afe92854a6
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-12-03 13:44:13 -0800

    turned off all scaling. currently set to hard-coded 800x600

--- www/js/main.js
+++ www/js/main.js
@@ -10,47 +10,70 @@
 //=====================
 var gameWidth = 800;
 var gameHeight = 600;
+// var aspectRatio = originalGameWidth/originalGameHeight;
+// var devicePixelRatio = window.devicePixelRatio;
+// var gameWidth = originalGameWidth * devicePixelRatio * aspectRatio;
+// var gameHeight = originalGameHeight * devicePixelRatio * aspectRatio;
+// console.log("gameWidth", gameWidth);
+// console.log("gameHeight", gameHeight);
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'game');
 function goFull() {
+    // game.scale.setMaximum();
+    // game.scale.updateLayout(true);
+
+
+
+
+
+
     // this code makes game show full screen but with blank bars on the sides
-    //===============================================================
-    if (this.game.device.desktop) {
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.scale.minWidth = gameWidth/2;
-        game.scale.minHeight = gameHeight/2;
-        game.scale.maxWidth = gameWidth * 1.5;
-        game.scale.maxHeight = gameHeight * 1.5;
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-        game.scale.updateLayout(true); // changed from this.scale.setScreenSize(true)
-    } else {
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.scale.minWidth = gameWidth/2;
-        game.scale.minHeight = gameHeight/2;
-        game.scale.maxWidth = gameWidth * 2.5;
-        game.scale.maxHeight = gameHeight * 2.5;
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-        game.scale.forceOrientation(true, false);
-        game.scale.hasResized.add(game.gameResized, game);
-        game.scale.enterIncorrectOrientation.add(game.enterIncorrectOrientation, game);
-        game.scale.leaveIncorrectOrientation.add(game.leaveIncorrectOrientation, game);
-        game.scale.updateLayout(true); // changed from this.scale.setScreenSize(true)
-    }
-    //==============================================================
-    // this code will scale up the canvas to make it fill the whole screen and eliminate bars on sides
-    var ow = parseInt(this.game.canvas.style.width, 10);
-    var oh = parseInt(this.game.canvas.style.height, 10);
-    var r = Math.max(window.innerWidth/ow, window.innerHeight/oh);
-    var nw = ow*r;
-    var nh = oh*r;
-    this.game.canvas.style.width = nw + 'px';
-    this.game.canvas.style.height = nh + 'px';
-    this.game.canvas.style.marginLeft = (window.innerWidth/2 - nw/2) + 'px';
-    this.game.canvas.style.marginTop = (window.innerHeight/2 - nh/2) + 'px';
-    document.getElementById('game').style.width = window.innerWidth + 'px';
-    document.getElementById('game').style.height = window.innerHeight - 1 + 'px'; //css for body includes 1px top margin that we want to eliminate
-    document.getElementById('game').style.overflow = 'hidden';
+    // //===============================================================
+    // if (this.game.device.desktop) {
+    //     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //     game.scale.minWidth = gameWidth/2;
+    //     game.scale.minHeight = gameHeight/2;
+    //     game.scale.maxWidth = gameWidth * 1.5;
+    //     game.scale.maxHeight = gameHeight * 1.5;
+    //     game.scale.pageAlignHorizontally = true;
+    //     game.scale.pageAlignVertically = true;
+    //     game.scale.updateLayout(true); // changed from this.scale.setScreenSize(true)
+    // } else {
+    //     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //     game.scale.minWidth = gameWidth/2;
+    //     game.scale.minHeight = gameHeight/2;
+    //     game.scale.maxWidth = gameWidth * 2.5;
+    //     game.scale.maxHeight = gameHeight * 2.5;
+    //     game.scale.pageAlignHorizontally = true;
+    //     game.scale.pageAlignVertically = true;
+    //     game.scale.forceOrientation(true, false);
+    //     game.scale.hasResized.add(game.gameResized, game);
+    //     game.scale.enterIncorrectOrientation.add(game.enterIncorrectOrientation, game);
+    //     game.scale.leaveIncorrectOrientation.add(game.leaveIncorrectOrientation, game);
+    //     game.scale.updateLayout(true); // changed from this.scale.setScreenSize(true)
+    // }
+    // //==============================================================
+    // // this code will scale up the canvas to make it fill the whole screen and eliminate bars on sides
+    // //=====
+    // var ow = parseInt(this.game.canvas.style.width, 10);
+    // var oh = parseInt(this.game.canvas.style.height, 10);
+    // var r = Math.max(window.innerWidth/ow, window.innerHeight/oh);
+    // var nw = ow*r;
+    // var nh = oh*r;
+    // this.game.canvas.style.width = nw + 'px';
+    // this.game.canvas.style.height = nh + 'px';
+    // this.game.canvas.style.marginLeft = (window.innerWidth/2 - nw/2) + 'px';
+    // this.game.canvas.style.marginTop = (window.innerHeight/2 - nh/2) + 'px';
+    // document.getElementById('game').style.width = window.innerWidth + 'px';
+    // document.getElementById('game').style.height = window.innerHeight - 1 + 'px'; //css for body includes 1px top margin that we want to eliminate
+    // document.getElementById('game').style.overflow = 'hidden';
+    // //====
+
+
+
+
+
+
+
 
     // these functions work with the device pixel ratio functions
     //=====================
