efaa21206e016314eaef68e3b18dbb9f15939138
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-12-07 12:59:35 -0800

    fixed credits page

--- src/Credits.js
+++ src/Credits.js
@@ -9,11 +9,11 @@ Credits.prototype = {
   },
 
   addCredit: function(task, author) {
-    var authorStyle = { font: game.width/20 + 'pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
-    var taskStyle = { font: game.width/26.6 + 'pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
+    var authorStyle = { font: this.game.width/20 + 'pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
+    var taskStyle = { font: this.game.width/26.6 + 'pt TheMinion', fill: 'white', align: 'center', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
     // change the y values for authorText and taskText to make text fields overlap or not when scrolling
-    var authorText = game.add.text(game.world.centerX, game.height/.667, author, authorStyle);
-    var taskText = game.add.text(game.world.centerX, game.height/.75, task, taskStyle);
+    var authorText = this.game.add.text(this.game.world.centerX, this.game.height/.667, author, authorStyle);
+    var taskText = this.game.add.text(this.game.world.centerX, this.game.height/.75, task, taskStyle);
     authorText.anchor.setTo(0.5);
     authorText.stroke = "rgba(0,0,0,0)";
     authorText.strokeThickness = 4;
@@ -22,14 +22,14 @@ Credits.prototype = {
     taskText.stroke = "rgba(0,0,0,0)";
     taskText.strokeThickness = 4;
     taskText.setShadow(3, 3, 'rgba(0,0,0,1.5)', 5);
-    game.add.tween(authorText).to( { y: -300 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 5000);
-    game.add.tween(taskText).to( { y: -200 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 5000);
+    this.add.tween(authorText).to( { y: -300 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 5000);
+    this.add.tween(taskText).to( { y: -200 }, 20000, Phaser.Easing.Cubic.Out, true, this.creditCount * 5000);
     this.creditCount ++;
   },
 
   addMenuOption: function(text, callback) {
-    var optionStyle = { font: game.height/20 + 'pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
-    var txt = game.add.text(game.width/30, ((this.optionCount + 2) * game.height/7.5) + game.height/2, text, optionStyle);
+    var optionStyle = { font: this.game.height/20 + 'pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
+    var txt = this.game.add.text(this.game.width/30, ((this.optionCount + 2) * this.game.height/7.5) + this.game.height/2, text, optionStyle);
 
     txt.stroke = "rgba(0,0,0,0";
     txt.strokeThickness = 4;
@@ -50,30 +50,30 @@ Credits.prototype = {
     txt.events.onInputOver.add(onOver, this);
     txt.events.onInputOut.add(onOut, this);
 
-    this.optionCount ++;
+    this.game.optionCount ++;
   },
 
   create: function () {
 
-    this.stage.disableVisibilityChange = true;
-    
+    this.game.stage.disableVisibilityChange = true;
+
     if (gameOptions.playMusic) {
 //      musicPlayer.stop();
 //      musicPlayer = game.add.audio('exit');
 //      musicPlayer.play();
     }
 
-    var bg = game.add.sprite(0, 0, 'gameover-bg');
+    var bg = this.game.add.sprite(0, 0, 'gameover-bg');
     this.addCredit('Physics Engineer', 'Shanna Sullivan');
     this.addCredit('State Engineer', 'Jazz Lyles');
     this.addCredit('Gameflow Engineer', 'Darryl Nunn');
     this.addCredit('Phaser.io & Cocoon', 'Powered By');
     this.addCredit('for playing', 'Thank you');
     this.addMenuOption('< Back', function (e) {
-      game.state.start("GameMenu");
+      this.game.state.start("MainMenu");
     });
 
-    game.add.tween(bg).to({alpha: 0}, 20000, Phaser.Easing.Cubic.Out, true, 10000);
+    this.game.add.tween(bg).to({alpha: 0}, 20000, Phaser.Easing.Cubic.Out, true, 10000);
   }
 
 };
