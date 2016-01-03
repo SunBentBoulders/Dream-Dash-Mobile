65da0f7135c4c1e81f78ae2e46e3c1677f6ca6f5
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-01-02 17:22:36 -0800

    loaded continue button and added continue button assets for directions screen

--- src/Directions.js
+++ src/Directions.js
@@ -5,10 +5,10 @@ var storyLine = 'Fast asleep and lost in a dream\nAvoid the ghoulies & wake up';
 var text, text2, titleText;
 
 Directions.prototype = {
-    
+
     preload: function(game){
         this.optionCount = 1;
-        
+
         if (window.deviceAssetSize === 'desktop') {
             game.load.image('directions-bg', 'assets/images/arrow_directions_crop.png');
             game.load.image('clock_candle', 'img/clock_candle_combo.png');
@@ -28,10 +28,16 @@ Directions.prototype = {
             game.load.image('directions-bg', 'assets/images/mobile_directions1408x792.png');
             game.load.image('clock_candle', 'img/clock_candle_combo.png');
         }
+        // load continue button specifically
+        if (window.deviceAssetSize === '960x640') {
+            game.load.image('Continue', 'assets/buttons/continueButton40pt.png');
+        } else {
+            game.load.image('Continue', 'assets/buttons/continueButton85pt.png');
+        }
     },
-    
+
     addDesktopMenuOption: function(text, callback) {
-        
+
         var optionStyle = {
             font: this.game.height / 20 + 'pt TheMinion',
             fill: '#f0daac',
@@ -39,7 +45,7 @@ Directions.prototype = {
             stroke: 'rgba(0,0,0,0)',
             strokeThickness: 4
         };
-        
+
         var txt = this.game.add.text(this.game.width / 30, ((this.optionCount + 2) * this.game.height / 7.5) + this.game.height / 4, text, optionStyle);
         txt.setShadow(3, 3, 'rgba(0,0,0,1.5)', 5);
         txt.stroke = "rgba(0,0,0,0";
@@ -50,13 +56,13 @@ Directions.prototype = {
             txt.useHandCursor = true;
             txt.setShadow(3, 3, 'rgba(0,0,0,1.5)', 5);
         };
-        
+
         var onOut = function(target) {
             target.fill = "#f0daac";
             target.stroke = "rgba(0,0,0,0)";
             txt.useHandCursor = false;
         };
-        
+
         txt.inputEnabled = true;
         txt.events.onInputUp.add(callback, this);
         txt.events.onInputOver.add(onOver, this);
@@ -71,12 +77,12 @@ Directions.prototype = {
         button.events.onInputDown.add(callback, this);
 
     },
-    
+
     create: function(game){
-        
+
         this.stage.backgroundColor = 0x000000;
         game.state.add('Preloader', Preloader);
-        
+
         if (this.game.device.desktop) {
 
             titleText = game.add.text(this.game.width/4, this.game.height.centerY/4, "How To Play", {
@@ -89,7 +95,7 @@ Directions.prototype = {
             var bg = game.add.sprite(this.game.width.centerX, this.game.height/6, 'directions-bg');
             var combo = game.add.sprite(this.game.width.centerX, this.game.height/8, 'clock_candle');
             combo.anchor.set(-.4,-.4);
-    
+
             text2 = game.add.text(this.game.width/2, this.game.height/6, directionsFullText, {
                 font: this.game.height / 40 + 'pt TheMinion',
                 fill: 'white',
@@ -110,7 +116,7 @@ Directions.prototype = {
 
             this.addDesktopMenuOption('Continue >', function(e) {
                     game.state.start("Preloader");
-            });      
+            });
         } else {
             titleText = game.add.text(this.game.width/2, this.game.height.centerY/4, "How To Play", {
                 font: 'bold ' + this.game.height / 20 + 'pt TheMinion',
@@ -118,10 +124,10 @@ Directions.prototype = {
                 align: 'center'
             });
             titleText.anchor.setTo(0);
-            
+
             game.add.sprite(this.game.width.centerX, this.game.height/4, 'directions-bg');
             game.add.sprite(this.game.width.centerX, this.game.height/8, 'clock_candle');
-    
+
             text2 = game.add.text(this.game.width/2, this.game.height/6, directionsFullText, {
                 font: this.game.width / 40 + 'pt TheMinion',
                 fill: 'white',
@@ -139,11 +145,11 @@ Directions.prototype = {
                 strokeThickness: 4
             });
             text.anchor.setTo(0);
-            
-			optionCount = 1;		
-	        this.addMobileMenuOption('Continue >', function() {
+
+			optionCount = 1;
+	        this.addMobileMenuOption('Continue', function() {
                 game.state.start('Preloader');
             })
-        }      
-    }  
+        }
+    }
 };
